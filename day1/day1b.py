e20c5c8179f26088f60adfe9476b580301dcd0b1
Mike MacKeigan <mmackeigan@gmail.com>
2022-12-30 09:42:03 -0700

    Updated code

--- day1/day1b.py
+++ day1/day1b.py
@@ -1,3 +1,11 @@
 # Day 1 - Part 2 - Calorie Counting - Top 3 carried calories combined
 
-d1_file = 'day1/day1_input.txt'
+x = open('day1/day1_input.txt').read().split('\n\n')
+
+e = {}
+y = 0
+for i in x:
+    e['output{0}'.format(y)] = sum(int(b) for b in i.splitlines())
+    y += 1
+
+print(sum(sorted(e.values(), reverse=True)[:3]))
