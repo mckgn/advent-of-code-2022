23ee7b425f07e23e2689ebfabdfc07e5834c8d11
Mike MacKeigan <mmackeigan@gmail.com>
2022-12-30 08:41:24 -0700

    Added txt file

--- day1/day1a.py
+++ day1/day1a.py
@@ -1,1 +1,12 @@
-print('Test')
+# Day 1 - Part 1 - Calorie Counting
+
+d1_file = 'day1_input.txt'
+
+with open(d1_file) as d1_f:
+    d1_contents = d1_f.read()
+
+e = {}
+for y in range(len(d1_contents.split('\n\n'))):
+    e["output{0}".format(y)] = sum((int(b) for b in d1_contents.split('\n\n')[y].replace('\n', ',').strip(',').split(',')))
+
+max(e.values())
