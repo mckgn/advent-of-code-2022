d8b136e007ec65c565bce01838f3a368ea9f28b3
Mike MacKeigan <mmackeigan@gmail.com>
2022-12-30 08:45:27 -0700

    Updated code

--- day1/day1a.py
+++ day1/day1a.py
@@ -1,6 +1,6 @@
 # Day 1 - Part 1 - Calorie Counting
 
-d1_file = 'day1_input.txt'
+d1_file = 'Z:/Projects/advent-of-code-2022/day1/day1_input.txt'
 
 with open(d1_file) as d1_f:
     d1_contents = d1_f.read()
@@ -9,4 +9,4 @@ e = {}
 for y in range(len(d1_contents.split('\n\n'))):
     e["output{0}".format(y)] = sum((int(b) for b in d1_contents.split('\n\n')[y].replace('\n', ',').strip(',').split(',')))
 
-max(e.values())
+print(max(e.values()))
