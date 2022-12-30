7fec3e6a925a973fb0db6c085bbdb9c6991e953f
Mike MacKeigan <mmackeigan@gmail.com>
2022-12-30 12:05:04 -0700

    Updated code

--- day1/day1a.py
+++ day1/day1a.py
@@ -2,10 +2,10 @@
 
 x = open('day1/day1_input.txt').read().split('\n\n')
 
-e = {}
-y = 0
+y = {}
+z = 0
 for i in x:
-    e['output{0}'.format(y)] = sum(int(b) for b in i.splitlines())
-    y += 1
+    y['output{0}'.format(z)] = sum(int(b) for b in i.splitlines())
+    z += 1
 
-print(max(e.values()))
+print(max(y.values()))
