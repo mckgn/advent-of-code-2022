9555a97ffaed8ca0fecccbf323ce988c1ee65eff
Mike MacKeigan <mmackeigan@gmail.com>
2023-01-02 00:25:21 -0700

    Rewrote without using Pandas

--- day2/day2b.py
+++ day2/day2b.py
@@ -1,10 +1,5 @@
 # Day 2 - Part 2 - Rock, Paper, Scissors - Scored based on corrected strategy guide
 
-# Using Pandas, plan on updating with a non Pandas way
-
-import pandas as pd
-import numpy as np
-
 x = open('day2/day2_input.txt').read().strip().splitlines()
 
 outcomes = {'A X' : 3, 'A Y' : 4, 'A Z' : 8, 'B X' : 1, 'B Y' : 5, 'B Z' : 9, 'C X' : 2, 'C Y' : 6, 'C Z' : 7}
