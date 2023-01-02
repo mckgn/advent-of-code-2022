441c4eda94655478ee40f073d334f15c3e1e0e68
Mike MacKeigan <mmackeigan@gmail.com>
2023-01-02 00:08:11 -0700

    Rewrote without using Pandas

--- day2/day2a.py
+++ day2/day2a.py
@@ -1,36 +1,12 @@
 # Day 2 - Part 1 - Rock, Paper, Scissors - Total score based on strategy guide
 
-# Using Pandas, plan on updating with a non Pandas way
-
-import pandas as pd
-import numpy as np
-
 x = open('day2/day2_input.txt').read().strip().splitlines()
 
-df = pd.DataFrame({'Matches' : x})
+outcomes = {'A X' : 4, 'A Y' : 8, 'A Z' : 3, 'B X' : 1, 'B Y' : 5, 'B Z' : 9, 'C X' : 7, 'C Y' : 2, 'C Z' : 6}
 
-df['Competitor'] = df['Matches'].astype(str).str[0]
-df['Me'] = df['Matches'].astype(str).str[2]
+total_score = 0
 
-df = df.replace('A', 'Rock')
-df = df.replace('B', 'Paper')
-df = df.replace('C', 'Scissors')
+for y in x:
+	total_score = total_score + outcomes[y]
 
-df = df.replace('X', 'Rock')
-df = df.replace('Y', 'Paper')
-df = df.replace('Z', 'Scissors')
-
-df['Outcome'] = np.where(df['Me'] == df['Competitor'], 'Tie', 'Lose')
-df['Outcome'] = np.where((df['Me'] == 'Rock') & (df['Competitor'] == 'Scissors'), 'Win', df['Outcome'])
-df['Outcome'] = np.where((df['Me'] == 'Paper') & (df['Competitor'] == 'Rock'), 'Win', df['Outcome'])
-df['Outcome'] = np.where((df['Me'] == 'Scissors') & (df['Competitor'] == 'Paper'), 'Win', df['Outcome'])
-
-df['Me Points'] = np.where(df['Me'] == 'Rock', 1, 3)
-df['Me Points'] = np.where(df['Me'] == 'Paper', 2, df['Me Points'])
-
-df['Outcome Points'] = np.where(df['Outcome'] == 'Win', 6, 3)
-df['Outcome Points'] = np.where(df['Outcome'] == 'Lose', 0, df['Outcome Points'])
-
-df['Total'] = df['Me Points'] + df['Outcome Points']
-
-print(df['Total'].sum())
+print(total_score)
